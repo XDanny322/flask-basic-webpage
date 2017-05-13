31e4da53586caa57323bc4d47ec7105b02e17475
Danny Lai <dlai@Dannys-MacBook-Pro.local>
2017-05-13 00:19:22 -0400

    Minor bugs | Added git ignore file

--- app/app.py
+++ app/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "blank page"
 
 ###############################################################################
 
