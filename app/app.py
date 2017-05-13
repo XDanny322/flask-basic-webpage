b865c7953a1815e3f507925ca5175f0fbc1f1592
Danny Lai <dlai@Dannys-MacBook-Pro.local>
2017-05-13 18:36:32 -0400

    Added first template

--- app/app.py
+++ app/app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # return render_template('index.html')
-    return "This is the main blank page."
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
